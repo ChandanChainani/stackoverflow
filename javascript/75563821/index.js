0271851456adbc6c6b69add4a5a0ed54f5ead2fc
ChandanChainani <chainanichan@gmail.com>
2023-05-01 10:29:23 +0530

    Link: https://stackoverflow.com/questions/75563821

---
+++ javascript/75563821/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+// console.log(process.env);
+
+// console.log('hello');
+
+process.env.USER_ID; // "239482"
+process.env.USER_KEY; // "foobar"
+process.env.NODE_ENV; // "development"
